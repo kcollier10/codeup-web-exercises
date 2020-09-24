75201f85cc5d62d4e919b299f32bb29b27fb6df1
Kristen Collier <kristencollier10@gmail.com>
2020-09-24 16:51:31 -0500

    Start of JavaScript with HTML lecture

---
+++ js/external-lec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("This is the external js file!");
+console.log("This is the second message!");
+
+var myAge = 50;
+console.log(myAge);
+
+alert("Message goes here!");
+
+alert("Codeup is really cool!");
+
+var confirmed = confirm("Are you sure you want to do this?");
+console.log(confirmed);
+
+var popup = confirm("Is this a pop-up?");
+console.log(popup);
+
+var userInput = prompt('Please type something :)');
+console.log('The user entered: ' + userInput);
+
+var message = prompt('How is your day going?');
+console.log('User has entered: ' + message);
---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Hello from inline JavaScript");
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
