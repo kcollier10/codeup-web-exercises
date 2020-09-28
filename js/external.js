7e238f2ba1b6f6880179279dd991aa4a81a680c6
Kristen Collier <kristencollier10@gmail.com>
2020-09-28 09:25:25 -0500

    first commit for warmups

--- js/external.js
+++ js/external.js
@@ -5,3 +5,5 @@ console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
 
+var color = prompt("What is your favorite color?");
+alert("Great! " + color + "is my favorite color, too!");
---
+++ js/warmup.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+console.log("Warm-up.js");
+console.log("Kristen");
+console.log("Kerri");
+console.log("Mark");
+console.log("Gerri");
+console.log("Chief");
