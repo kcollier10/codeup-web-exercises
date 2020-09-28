71854973c425bab43b69a2d94307b0cea64649e4
Kristen Collier <kristencollier10@gmail.com>
2020-09-28 09:34:57 -0500

    warmup updates

--- js/warmup.js
+++ js/warmup.js
@@ -7,3 +7,11 @@ console.log("Kerri");
 console.log("Mark");
 console.log("Gerri");
 console.log("Chief");
+
+function sayHello(name){
+    console.log("Hello " + name);
+    // we will talk about returning later!
+}
+
+sayHello("Chief");
+sayHello("Kristen");
