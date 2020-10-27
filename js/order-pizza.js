cf38ecb4a7e598386e661b7d573db5a9ba4fa5b6
Kristen Collier <kristencollier10@gmail.com>
2020-10-27 16:16:39 -0500

    pizza exercise

---
+++ js/order-pizza.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#myTab li:last-child a').tab('show')
+})
+
+// $(function () {
+//     $('#toppings')
+// })
+
+// $('#myTab a[href="#size"]').tab('show');
+// $('#myTab a[href="#cheese-sauce"]').tab('show');
+// $('#myTab a[href="#toppings"]').tab('show');
