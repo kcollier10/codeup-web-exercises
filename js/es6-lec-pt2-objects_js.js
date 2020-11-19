cdeb9d71b27dee730a72d2270cc22de2c9a7071d
Kristen Collier <kristencollier10@gmail.com>
2020-11-19 16:01:45 -0600

    second lecture notes

--- js/es6-lec-pt2-objects_js.js
+++ js/es6-lec-pt2-objects_js.js
@@ -107,7 +107,7 @@
 /*==============================================*/
 //Destructuring with Arrays
 
-let cats = ["CJ", "Claude", "Max"];
+// let cats = ["CJ", "Claude", "Max"];
 
 //oldway
 // let cat1 = cats[0];
@@ -115,8 +115,8 @@ let cats = ["CJ", "Claude", "Max"];
 // let cat3 = cats[2];
 
 
-//new way
-let [ cat1, cat2, cat3 ] = cats;
+// new way
+// let [ cat1, cat2, cat3 ] = cats;
 
 // console.log(cat1);
 // console.log(cat2);
@@ -135,25 +135,29 @@ const alphabet = ['A', 'B', 'C', 'D', 'E'];
 
 //TODO TOGETHER: new way
 
-let [ a, b, c, d, e ] = alphabet;
-
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
+// let [ a, b, c, d, e ] = alphabet;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
 
 
 
 //TODO TOGETHER: To skip an element...
 
-
+// const[a, ,c, d] = alphabet;
 
 
 
 //TODO TOGETHER: To get all elements...Spread operator *BONUS
 
-
+// const[a, b, c, ...rest ] = alphabet;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(rest);
 
 
 
